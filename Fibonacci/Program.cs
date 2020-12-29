949d09fe10da64902a8b2c518411380f96c967c6
Roman Zdorovenko <75687097+ZdorovenkoR@users.noreply.github.com>
2020-12-30 00:11:33 +0600

    Update Program.cs

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//Написать программу, вычисляющую число Фибоначчи для заданного значения
+//рекурсивным способом.
 
 namespace Fibonacci
 {
@@ -10,6 +12,21 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите число итераций числа Фибоначчи");
+            int iteration = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Число Фибоначчи: {0}",Fibonacci(iteration));
+            Console.ReadKey();
+        }
+        static int Fibonacci(int iteration)
+        {
+            if (iteration == 0 || iteration == 1)
+            {
+                return (iteration);
+            }
+            else
+            {
+                return Fibonacci(iteration - 1) + Fibonacci(iteration - 2);
+            }
         }
     }
 }
